3ed56d226523128faf762fe542b3728b3a2270a4
jonas ostlund <jo.ostlund@gmail.com>
2017-06-03 18:29:51 +0200

    Inital commit. Skeleton app.

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+var path = require('path');
+
+app.use('/static', express.static('public'));
+app.use('/modules', express.static('node_modules'));
+
+app.use(function (req, res, next) {
+  console.log('Request:', req.url);
+  next()
+})
+app.get('/index', function (req, res) {
+  //res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/public/views/index.html'));
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
